bebe218d921557e273660aca58632a58178970ce
YudiR <yudrose13@gmail.com>
2019-02-28 22:22:44 -0500

    Completed index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get  '/fishes' => 'fishes#index'
+get '/fishes/:id' => 'fishes#show'
 end
